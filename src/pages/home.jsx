cb65d9393dfa354001f2a6ed0a1a1864a0e0918a
Glitch (glitch-blank-react) <none>
2025-03-04 15:01:28 +0000

    😋🐬 Checkpoint ./src/pages/home.jsx:75721292/236

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,9 +9,7 @@ export default function Home() {
     <>
       <h1 className="title">{hello}</h1>
       <p>
-        This starter gives you everything you need to start working on a new
-        React project, and nothing more. If you're looking for something with
-        more options included out of the box, try remixing
+        Project for reading in recipes.
         {" "}
         <a href="https://glitch.com/edit/#!/remix/glitch-hello-react">
           ~glitch-hello-react
